75608a653ffc084008bccbc0fd9cdd4aeb2421d0
Htet Aung Shine <has.htetaungshine1994@gmail.com>
2016-11-29 20:57:12 +0800

    Added Hot Potato Game

---
+++ ds/queue/hot_potato.py
@@ -0,0 +1,17 @@
+from queue import Queue
+
+def play(nameList, num):
+    q = Queue()
+    for x in nameList:
+        q.enqueue(x)
+    while q.size() > 1:
+        for i in range(num):
+            q.enqueue(q.dequeue())
+        q.dequeue()
+    return q.dequeue()
+
+def main():
+    print("%s" % play(["Bill", "Brad", "Jane", "John", "Lindsay", "Joyce"], 7))
+
+if __name__ == "__main__":
+    main()
